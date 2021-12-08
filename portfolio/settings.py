75d4b40f88b556022320266a9d7a5982d58fcfcf
Dickson-Worlako-Dzokoto <worlako1986@hotmail.com>
2021-12-08 23:47:58 +0000

    Add Model

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,12 +79,18 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portfolio',
+        'USER': 'lite',
+        'PASSWORD': '123456789@',
+        'HOST': 'localhost',
+        'PORT': '5433',
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
