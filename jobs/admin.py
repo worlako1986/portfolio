8550b9f11a8b17ebc95742e0f90f9feb05e9c3fe
Dickson-Worlako-Dzokoto <worlako1986@hotmail.com>
2021-12-09 11:22:06 +0000

    Add new model and static files

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 
-from jobs.models import Job
+from jobs.models import Job, Person
 
 # Register your models here.
 
 class JobModelAdmin(admin.ModelAdmin):
-    list_display = ["summery", "image"]
+    list_display = ["person","summery", "image"]
 
+class PersonModelAdmin(admin.ModelAdmin):
+    list_display = ["name", "about", "image"]
 
 admin.site.register(Job, JobModelAdmin)
+admin.site.register(Person, PersonModelAdmin)
--- jobs/views.py
+++ jobs/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from jobs.models import Job, Person
 
 
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = "jobs/index.html"
-    extra_context = {}
+    jobs =  Job.objects.all()
+    extra_context = {"jobs":jobs}
+
+
+def home(request):
+
+    person = Person.objects.get(pk = 1)
+    jobs = Job.objects.filter(person = person)
+
+    return render(request, "jobs/index.html", {
+        "jobs": jobs,
+        "person":person
+    })
